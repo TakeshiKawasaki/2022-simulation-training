9b85a7413a01c5d23050c4446b1d663b81593f77
TakeshiKawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2022-05-05 17:40:14 +0900

    Update analyze.cpp

--- Lecture5/analyze.cpp
+++ Lecture5/analyze.cpp
@@ -7,28 +7,27 @@
 #include "BM.h"
 
 #define tmax 100
-#define temp 1.
+#define temp 0.01
 #define dt 0.01
 #define ensemble 1000
-#define twsize 52
+#define window 52
 #define dim 3
 //using namespace std;
 
 void ini(double *dr2,double *corr){
-  for(int i=0;i<twsize;i++){
+  for(int i=0;i<window;i++){
     dr2[i]=0.0;
     corr[i]=0.0;
   }
 }
-void input(double (*x)[ensemble*twsize],double (*v)[ensemble*twsize],double *t){
+void input(double (*x)[dim],double (*v)[dim],double *t){
   char filename[128];
   std::ifstream file;
   sprintf(filename,"coord_dt%.3fT%.3f.dat",dt,temp);
   file.open(filename);
-  int asize=ensemble*twsize;
+  int asize=ensemble*window;
   for(int i=0;i<asize;i++){
-    file >> t[i] >> x[0][i] >> x[1][i] >> x[2][i];
-    std::cout << t[i] <<"\t"<<x[0][i]<<"\t"<<x[1][i]<<"\t"<<x[2][i]<<std::endl;
+    file >> t[i] >> x[i][0] >> x[i][1] >> x[i][2];
   }
   file.close();
 
@@ -47,24 +46,24 @@ void output(double *t,double *dr2,double *corr){
   std::ofstream file;
   sprintf(filename,"msd%.3fT%.3f.dat",dt,temp);
   file.open(filename);
-  for(int i=0;i<twsize;i++)
+  for(int i=0;i<window;i++)
     file<<t[i]-t[0]<<"\t"<<dr2[i]<<std::endl;
   file.close();
 
   sprintf(filename,"corr_dt%.3fT%.3f.dat",dt,temp);
   file.open(filename);
-  for(int i=0;i<twsize;i++)
+  for(int i=0;i<window;i++)
     file<<t[i]-t[0]<<"\t"<<corr[i]<<std::endl;
   file.close();
 }
 
-void analyze(double (*x)[ensemble*twsize],double (*v)[ensemble*twsize],double *t,double *dr2,double *corr){
+void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr){
   double dx[dim],corr_x[dim];
   for(int i=0;i<ensemble;i++)
-    for(int j=0;j<twsize;j++){
+    for(int j=0;j<window;j++){
       for(int k=0;k<dim;k++){
-	dx[k]=(x[k][j+twsize*i]-x[k][twsize*i]); //k=0:x, k=1:y, k=2:z
-	corr_x[k]=v[k][j+twsize*i]*v[k][twsize*i];
+	dx[k]=(x[j+window*i][k]-x[window*i][k]);
+	corr_x[k]=v[j+window*i][k]*v[window*i][k];
 	dr2[j]+=(dx[k]*dx[k])/ensemble;
 	corr[j]+=(corr_x[k])/ensemble;
       }
@@ -72,7 +71,7 @@ void analyze(double (*x)[ensemble*twsize],double (*v)[ensemble*twsize],double *t
 }
 
 int main(){
-  double x[dim][ensemble*twsize],v[dim][ensemble*twsize],t[ensemble*twsize],dr2[twsize],corr[twsize];
+  double x[ensemble*window][dim],v[ensemble*window][dim],t[ensemble*window],dr2[window],corr[window];
   int i,j;
   ini(dr2,corr);
   input(x,v,t);
