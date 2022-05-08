61a39b41a4358408767e301c1bf2608ea15f85a5
TakeshiKawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2022-05-08 11:48:58 +0900

    Update analyze.cpp

--- Lecture5/analyze.cpp
+++ Lecture5/analyze.cpp
@@ -7,10 +7,10 @@
 #include "BM.h"
 
 #define tmax 100
-#define temp 1.0
+#define temp 0.01
 #define dt 0.01
-#define ensemble 1000
-#define window 52
+#define ensemble 10000
+#define window 39
 #define dim 3
 //using namespace std;
 
@@ -20,6 +20,7 @@ void ini(double *dr2,double *corr){
     corr[i]=0.0;
   }
 }
+
 void input(double (*x)[dim],double (*v)[dim],double *t){
   char filename[128];
   std::ifstream file;
@@ -38,10 +39,9 @@ void input(double (*x)[dim],double (*v)[dim],double *t){
     // std::cout << t[i] <<"\t"<<v[0][i]<<"\t"<<v[1][i]<<"\t"<<v[2][i]<<std::endl;
   }
   file.close();
-
 }
 
-void output(double *t,double *dr2,double *corr){
+void output(double *t,double *dr2,double *corr,double integral){
   char filename[128];
   std::ofstream file;
   sprintf(filename,"msd_dt%.3fT%.3f.dat",dt,temp);
@@ -55,9 +55,11 @@ void output(double *t,double *dr2,double *corr){
   for(int i=1;i<window;i++)
     file<<t[i]-t[0]<<"\t"<<corr[i]<<std::endl;
   file.close();
+  
+  std::cout<<"A result of the integral ="<<integral<<std::endl;
 }
 
-void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr){
+void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr,double *integral){
   double dx[dim],corr_x[dim];
   for(int i=0;i<ensemble;i++)
     for(int j=0;j<window;j++){
@@ -66,16 +68,21 @@ void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *cor
 	corr_x[k]=v[j+window*i][k]*v[window*i][k];
 	dr2[j]+=(dx[k]*dx[k])/ensemble;
 	corr[j]+=(corr_x[k])/ensemble;
+	if(j!=window-1)
+	  *integral+=0.5*(v[j+window*i][k]*v[window*i][k]+v[j+1+window*i][k]*v[window*i][k])*(t[j+1]-t[j])/ensemble;
       }
     }
 }
 
 int main(){
-  double x[ensemble*window][dim],v[ensemble*window][dim],t[ensemble*window],dr2[window],corr[window];
+  double t[ensemble*window],dr2[window],corr[window],integral=0.0;
   int i,j;
+  double  (*x)[dim] = new double[ensemble*window][dim];
+  double  (*v)[dim] = new double[ensemble*window][dim]; 
+  
   ini(dr2,corr);
   input(x,v,t);
-  analyze(x,v,t,dr2,corr);
-  output(t,dr2,corr);
+  analyze(x,v,t,dr2,corr,&integral);
+  output(t,dr2,corr,integral);
   return 0;
 }
